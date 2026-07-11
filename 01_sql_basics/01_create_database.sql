4aadda43a00fd61aa9753e255c2ad37719b29973
Khan Jarjish <jarjishkhan695@gmail.com>
2026-07-11 15:46:51 +0530

    Add create database SQL file

---
+++ 01_sql_basics/01_create_database.sql
@@ -0,0 +1,9 @@
+-- Creating a database
+-- This file uses MySQL syntax.
+
+CREATE DATABASE IF NOT EXISTS sql_journey;
+
+USE sql_journey;
+
+-- SHOW DATABASES displays all databases available on the server.
+SHOW DATABASES;
